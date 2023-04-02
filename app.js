5a49ddd58c824faa5560478bdd7fdf23cab269b8
Jiangwei-shi <56906163+Jiangwei-shi@users.noreply.github.com>
2023-04-02 02:32:28 -0400

    finished part 4

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import HelloController
-  from "./controllers/hello-controller.js"
-import UserController
-  from "./controllers/users/users-controller.js"
+import express from "express";
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
+
 const app = express();
-HelloController(app)
-UserController(app)
-app.listen(4000)
+app.use(express.json());
+HelloController(app);
+UserController(app);
+app.listen( 4000);
--- controllers/hello-controller.js
+++ controllers/hello-controller.js
@@ -1,9 +1,11 @@
 const HelloController = (app) => {
-  app.get('/hello', (req, res) => {
-    res.send('Life is good!')
-  })
-  app.get('/', (req, res) => {
-    res.send('Welcome to Full Stack Development!')
-  })
-}
+  app.get("/hello", (req, res) => {
+    res.send("Life is good!");
+  });
+
+  app.get("/", (req, res) => {
+    res.send("Welcome to Full Stack Development!");
+  });
+};
+
 export default HelloController;

2ad8ac29eefc513202e3946e9893fd6887bfd99d
Jiangwei-shi <56906163+Jiangwei-shi@users.noreply.github.com>
2023-04-09 22:48:36 -0400

    finished section 3

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -2,46 +2,35 @@ import * as tuitsDao from '../../tuits/tuits-dao.js'
 // import posts from "./tuits.js";
 // let tuits = posts;
 
-const createTuit = async(req, res) => {
+const createTuit = async (req, res) => {
   const newTuit = req.body;
-  // newTuit._id = new Date().getTime() + "";
   newTuit.likes = 0;
   newTuit.liked = false;
-  newTuit.dislikes = 0;
-  newTuit.disliked = false;
-  newTuit.image = "nasa.svg";
-  // tuits.push(newTuit);
   const insertedTuit = await tuitsDao
   .createTuit(newTuit);
-  res.json(newTuit);
-};
+  res.json(insertedTuit);
+}
 
-const findTuits = async(req, res) => {
+const findTuits = async (req, res) => {
   const tuits = await tuitsDao.findTuits()
   res.json(tuits);
-};
+}
 
-const updateTuit = async(req, res) => {
+const updateTuit = async (req, res) => {
   const tuitdIdToUpdate = req.params.tid;
   const updates = req.body;
-  // const tuitIndex = tuits.findIndex((t) => t._id === tuitdIdToUpdate);
-  // tuits[tuitIndex] = {
-  //   ...tuits[tuitIndex],
-  //   ...updates,
-  // };
   const status = await tuitsDao
   .updateTuit(tuitdIdToUpdate,
     updates);
   res.json(status);
-};
+}
 
-const deleteTuit = async(req, res) => {
+const deleteTuit = async (req, res) => {
   const tuitdIdToDelete = req.params.tid;
   const status = await tuitsDao
   .deleteTuit(tuitdIdToDelete);
-  // tuits = tuits.filter((t) => t._id !== tuitdIdToDelete);
   res.json(status);
-};
+}
 
 export default (app) => {
   app.post("/api/tuits", createTuit);

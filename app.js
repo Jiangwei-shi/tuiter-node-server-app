5a0e67ea51336efa6810d9c23d50da465331e746
Jiangwei-shi <56906163+Jiangwei-shi@users.noreply.github.com>
2023-04-09 20:39:13 -0400

    finished section2

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController
   from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 
 
 const app = express();
